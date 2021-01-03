e554fb7108d39946e63e43e7bfc547942b7ecc20
Noah Vaillancourt <noah.vaillancourt@gmail.com>
2021-01-03 15:13:47 -0500

    create room and join room logic workin on home page
    also added routing to the room page. Will finish at a later date

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,86 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import {Flex, Spacer, Box, Heading, Text, Input, Button} from '@chakra-ui/react';
 import React from 'react'
+import {gql, useMutation} from '@apollo/client'
+import { useRouter, Router } from 'next/router'
+
+//graphql data fetching
+const UPDATE_JOIN_ROOM = gql`
+  mutation updatedJoinRoom($steamid: String, $roomcode: String) {
+    updatedJoinRoom(steamid: $steamid, roomcode: $roomcode){
+      id
+      memberIds
+      inviteCode
+    }
+  }
+`
+const UPDATE_CREATE_ROOM = gql`
+  mutation updatedCreateNewRoom($steamid: String){
+    updatedCreateNewRoom(steamid: $steamid){
+      id
+      inviteCode
+    }
+  }
+`
+
+
+
 
 export default function Home() {
-  const [roomcode, setValue] = React.useState("")
-  const handleRoomCode = (event) => setValue(event.target.value)
+  const router = useRouter()
+  const [roomcode, setRoomCode] = React.useState("")
+  const handleRoomCode = (event) => setRoomCode(event.target.value)
+
+  const [joinSteamID, setJoinID] = React.useState("")
+  const handleJoinID = (event) => setJoinID(event.target.value)
+
+  const [createSteamID, setCreateID] = React.useState("")
+  const handleCreateID = (event) => setCreateID(event.target.value)
+
+  const [joinRoom, {data}] = useMutation(UPDATE_JOIN_ROOM)
+  const joinRoomLogic = () => {
+    joinRoom({
+      variables: {
+        steamid: joinSteamID.toString(),
+        roomcode: roomcode
+      }
+    })
+    router.push({
+      pathname: '/room/[roomcode]',
+      query: {roomcode: roomcode.replace('#', '')}
+    })
+  }
+
+  const [createRoom, {room}] = useMutation(UPDATE_CREATE_ROOM,{
+    onCompleted: async(room) => {
+      console.log(room)
+      router.push({
+        pathname: '/room/[roomcode]',
+        query: {roomcode: room.updatedCreateNewRoom.inviteCode.replace('#','')}
+      })
+    }
+  })
+  const createRoomLogic = () => {
+    console.log(createSteamID.toString())
+    createRoom({
+      variables: {
+        steamid: createSteamID.toString()
+      }
+    })
+
+  }
+
+
   return (
     <Flex direction="column" align="center">
       <Heading as="h1">Tinder For Gamers - Beta</Heading>
       <Box borderWidth="1px" borderRadius="lg" rounded="lg">
         <Flex direction="column" align="center">
-          <Input size="sm" placeholder="Steam ID"></Input>
+          <Input size="sm" placeholder="Steam ID" value={joinSteamID} onChange={handleJoinID}></Input>
           <Input size="sm" placeholder="Invite Code" value={roomcode} onChange={handleRoomCode}></Input>
-          <Button>Join Room</Button>
-          <Input size="sm" placeholder="Steam ID"></Input>
-          <Button>Create Room</Button>
+          <Button onClick={() => joinRoomLogic()}>Join Room</Button>
+          <Input size="sm" placeholder="Steam ID" value={createSteamID} onChange={handleCreateID}></Input>
+          <Button onClick={()=> createRoomLogic()}>Create Room</Button>
         </Flex>
       </Box>
     </Flex>
