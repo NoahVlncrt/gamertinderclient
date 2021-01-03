42557f72f3691eea8b25295c059a0639e31d7ea0
Noah Vaillancourt <noah.vaillancourt@gmail.com>
2021-01-03 13:44:32 -0500

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql} from '@apollo/client'
+import { ChakraProvider } from "@chakra-ui/react"
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return  (
+    <ChakraProvider>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
