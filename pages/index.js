a5c33d6a250a4d76564833a374d6945679ce47a5
Adam <adammurdocknh@gmail.com>
2021-01-04 02:46:22 -0500

    Finished design for LandingPage, JoinPage, and CreatePage. Functionality with state needs to be added and is noted in comments as such. Beginning work on room page but my 'go-go' juice is out and it's almost 3am.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
+import Next from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Flex, Spacer, Box, Heading, Text, Input, Button, Container, Center, Stack, Divider } from '@chakra-ui/react';
 import Image from 'next/image'
 import React from 'react'
-import {gql, useMutation} from '@apollo/client'
+import { gql, useMutation } from '@apollo/client'
 import { useRouter, Router } from 'next/router'
 
 //graphql data fetching
@@ -27,6 +28,59 @@ const UPDATE_CREATE_ROOM = gql`
 
 
 
+// TODO: Add Functionality of buttons
+export function LandingPage() {
+  return (
+    <Stack direction="column" align="center" size="xl">
+      <Text fontFamily="Oxygen" fontSize="lg" fontWeight="bold" fontStyle="italic">Get Started:</Text>
+      <Button colorScheme="red" width="xs" fontFamily="Oxygen">Join Room</Button>
+      <Button colorScheme="red" width="xs" fontFamily="Oxygen">Create Room</Button>
+    </Stack>
+  )
+}
+
+export function JoinPage() {
+  return (
+    <Stack direction="column" align="center" size="xl">
+      {/* CODE FROM STARTING COMMIT. DOES NOT FUNCTION BECAUSE OF STATE*/}
+      {/*<Input size="sm" placeholder="Steam ID" value={joinSteamID} onChange={handleJoinID}></Input>
+      <Input size="sm" placeholder="Invite Code" value={roomcode} onChange={handleRoomCode}></Input>
+      <Button onClick={() => joinRoomLogic()}>Join Room</Button> */}
+      <Text fontFamily="Oxygen" fontSize="lg" fontWeight="bold" fontStyle="italic">Join Room:</Text>
+      <Input width="xs" fontFamily="Oxygen" placeholder="Steam ID" />
+      <Input width="xs" fontFamily="Oxygen" placeholder="Invite Code" />
+      <Button colorScheme="red" width="xs" fontFamily="Oxygen">Submit</Button>
+    </Stack>
+  )
+}
+
+export function CreatePage() {
+  return (
+    <Stack direction="column" align="center" size="xl">
+      {/* CODE FROM STARTING COMMIT. DOES NOT FUNCTION BECAUSE OF STATE*/}
+      {/* 
+      <Input size="sm" placeholder="Steam ID" value={createSteamID} onChange={handleCreateID}></Input>
+            <Button onClick={() => createRoomLogic()}>Create Room</Button> */}
+      <Text fontFamily="Oxygen" fontSize="lg" fontWeight="bold" fontStyle="italic">Create Room:</Text>
+      <Input width="xs" fontFamily="Oxygen" placeholder="Steam ID" />
+      <Button colorScheme="red" width="xs" fontFamily="Oxygen">Submit</Button>
+    </Stack>
+  )
+}
+
+export function RoomPage() {
+  return (
+    <Flex direction="column" align="center" bg="#f1f7fc" p={5}>
+      <Flex direction='column' bg="white" width="6xl" border="true" borderRadius={10} borderWidth={5}>
+        <Flex direction="row" justifyContent="start" alignItems="center" p={5}>
+          <Image src="/logo.png" width={100} height={100} p={5} />
+          <Button colorScheme="red"></Button>
+          <Button colorScheme="blue"></Button>
+        </Flex>
+      </Flex>
+    </Flex >
+  )
+}
 
 export default function Home() {
   const router = useRouter()
@@ -39,7 +93,7 @@ export default function Home() {
   const [createSteamID, setCreateID] = React.useState("")
   const handleCreateID = (event) => setCreateID(event.target.value)
 
-  const [joinRoom, {data}] = useMutation(UPDATE_JOIN_ROOM)
+  const [joinRoom, { data }] = useMutation(UPDATE_JOIN_ROOM)
   const joinRoomLogic = () => {
     joinRoom({
       variables: {
@@ -49,16 +103,16 @@ export default function Home() {
     })
     router.push({
       pathname: '/room/[roomcode]',
-      query: {roomcode: roomcode.replace('#', '')}
+      query: { roomcode: roomcode.replace('#', '') }
     })
   }
 
-  const [createRoom, {room}] = useMutation(UPDATE_CREATE_ROOM,{
-    onCompleted: async(room) => {
+  const [createRoom, { room }] = useMutation(UPDATE_CREATE_ROOM, {
+    onCompleted: async (room) => {
       console.log(room)
       router.push({
         pathname: '/room/[roomcode]',
-        query: {roomcode: room.updatedCreateNewRoom.inviteCode.replace('#','')}
+        query: { roomcode: room.updatedCreateNewRoom.inviteCode.replace('#', '') }
       })
     }
   })
@@ -74,17 +128,36 @@ export default function Home() {
 
 
   return (
-    <Flex direction="column" align="center">
-      <Heading as="h1">Tinder For Gamers - Beta</Heading>
-      <Box borderWidth="1px" borderRadius="lg" rounded="lg">
-        <Flex direction="column" align="center">
-          <Input size="sm" placeholder="Steam ID" value={joinSteamID} onChange={handleJoinID}></Input>
-          <Input size="sm" placeholder="Invite Code" value={roomcode} onChange={handleRoomCode}></Input>
-          <Button onClick={() => joinRoomLogic()}>Join Room</Button>
-          <Input size="sm" placeholder="Steam ID" value={createSteamID} onChange={handleCreateID}></Input>
-          <Button onClick={()=> createRoomLogic()}>Create Room</Button>
-        </Flex>
-      </Flex>
-    </Flex >
+    <RoomPage />
+    // <Flex direction="column" align="center" bg="#f1f7fc" p={5}>
+    //   <Flex direction='column' align="center" bg="white" size="lg" border="true" borderRadius={10} borderWidth={5}>
+    //     <Stack align="center" p={5}>
+    //       <Image src="/logo.png" width={100} height={100} size="lg" p={5} />
+    //       <Spacer size="lg" />
+    //       <Heading as="h1" size="lg" fontFamily="Ubuntu">Tinder For Gamers - Beta</Heading>
+    //       <Spacer size="lg" />
+    //       {/* SUPER RATCHET DIV */}
+    //       <Flex bg="#e3e8ef" borderRadius={10} h={1} w={350}></Flex>
+    //       <Spacer size="lg" />
+    //       <Flex direction="column" justify="center">
+
+    //         {/* FOR TESTING PURPOSES */}
+    //         {/* <LandingPage /> */}
+
+    //         {/* <JoinPage /> */}
+
+    //         {/* <CreatePage /> */}
+
+
+
+    //         {/* <Input size="sm" placeholder="Steam ID" value={joinSteamID} onChange={handleJoinID}></Input>
+    //         <Input size="sm" placeholder="Invite Code" value={roomcode} onChange={handleRoomCode}></Input>
+    //         <Button onClick={() => joinRoomLogic()}>Join Room</Button>
+    //         <Input size="sm" placeholder="Steam ID" value={createSteamID} onChange={handleCreateID}></Input>
+    //         <Button onClick={() => createRoomLogic()}>Create Room</Button> */}
+    //       </Flex>
+    //     </Stack>
+    //   </Flex>
+    // </Flex >
   )
 }
